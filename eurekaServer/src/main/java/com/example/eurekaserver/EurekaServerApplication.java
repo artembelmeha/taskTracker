504ee2d7593db4be30d46dd193a49643c6478571
AB090137 <artem_belmeha@epam.com>
2021-11-20 19:38:50 +0200

    refactoring call api calls work via zuul

--- eurekaServer/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eurekaServer/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -1,8 +1,16 @@
 package com.example.eurekaserver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +18,14 @@ public class EurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
     }
 
 }
--- zuulServer/src/main/java/com/example/zuulserver/ZuulServerApplication.java
+++ zuulServer/src/main/java/com/example/zuulserver/ZuulServerApplication.java
@@ -1,8 +1,16 @@
 package com.example.zuulserver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,4 +20,11 @@ public class ZuulServerApplication {
         SpringApplication.run(ZuulServerApplication.class, args);
     }
 
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
+    }
 }
