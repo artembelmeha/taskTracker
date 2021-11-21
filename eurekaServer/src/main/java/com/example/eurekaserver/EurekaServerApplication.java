8d249bc7e7b92b3edfa04af8155e9f836d6114d4
AB090137 <artem_belmeha@epam.com>
2021-11-21 11:35:36 +0200

    added Trace Id

--- eurekaServer/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eurekaServer/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.client.LinkDiscoverer;
@@ -14,6 +15,7 @@ import org.springframework.plugin.core.SimplePluginRegistry;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
--- zuulServer/src/main/java/com/example/zuulserver/ZuulServerApplication.java
+++ zuulServer/src/main/java/com/example/zuulserver/ZuulServerApplication.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.client.LinkDiscoverer;
@@ -12,6 +13,7 @@ import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
 
+@ServletComponentScan
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulServerApplication {
