1badc9de2f645db6ac8627af675ade3540d72691
AB090137 <artem_belmeha@epam.com>
2021-11-25 19:07:34 +0200

    small updates

--- usersService/src/main/java/com/test/task/tasktracker/repository/UserRepository.java
+++ usersService/src/main/java/com/test/task/tasktracker/repository/UserRepository.java
@@ -1,13 +1,18 @@
 package com.test.task.tasktracker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.test.task.tasktracker.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Transactional
+    @Modifying
     @Query(value = "delete from user_my_tasks where my_tasks = ?1", nativeQuery = true)
     void deleteTaskFromUserByTaskId(long myTaskId);
 }
